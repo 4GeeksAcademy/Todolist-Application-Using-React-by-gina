e634bd6b69b05f779589a5680d10e17bad0bde87
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-11 15:33:24 +0000

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      const newTodo = {
+        id: Date.now(),
+        text: inputValue
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="notebook my-5">
+      <h1 className="title">To Do</h1>
+      <ul className="todo-list">
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={handleKeyPress}
+            placeholder="What to do"
+            className="input-task"
+          />
+        </li>
+        {todos.length === 0 ? (
+          <li className="empty-task">No tasks, add tasks</li>
+        ) : (
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.text}</span>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(todo.id)}
+              >
+                X
+              </button>
+            </li>
+          ))
+        )}
+      </ul>
+      <div className="task-count">{todos.length} item left</div>
+    </div>
+  );
 };
 
 export default Home;
